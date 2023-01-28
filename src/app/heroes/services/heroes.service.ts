2171ba71acb968982160d529a670eacf2f66975f
secondary-jcav <secondary.jcav@gmail.com>
2023-01-28 10:43:35 +0100

    added url in environments files

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,19 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Hero } from '../interfaces/heroes.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
+  private baseUrl: string = environment.baseUrl;
   constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>('http://localhost:3000/heroes');
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
   getOneHero(id: string): Observable<Hero> {
-    return this.http.get<Hero>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`);
   }
 }
